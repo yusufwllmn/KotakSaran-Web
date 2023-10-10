fd1230cf3f953333072cd7b2306f195b0cb7469b
DaffaAriya <daffaariyaprayoga@gmail.com>
2023-10-10 09:20:05 +0700

    add package

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
-use Illuminate\Support\Facades\Auth;
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,34 +11,28 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/home', function () {
-        if(Auth::user()->role == 'admin'){
-            return redirect('/admin/dashboard');
-        } else if(Auth::user()->role == 'petugas'){
-            return redirect('/petugas/dashboard');
-        } else if(Auth::user()->role == 'pelapor'){
-            return redirect('/pelapor/dashboard');
-        } else{
-            return redirect('/');
-        }
-    });
-    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-    Route::middleware(['role:admin'])->group(function() {
-        Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('adminPage');
-    });
-
-    Route::middleware(['role:petugas'])->group(function() {
-        Route::get('/petugas/dashboard', [AdminController::class, 'index'])->name('peugasPage');
-    });
-
-    Route::middleware(['role:pelapor'])->group(function() {
-        Route::get('/pelapor/dashboard', [AdminController::class, 'index'])->name('pelaporPage');
-    });
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
