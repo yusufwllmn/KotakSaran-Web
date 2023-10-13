d394f7685bdafe5e8b720be6192f588fe577e451
DaffaAriya <daffaariyaprayoga@gmail.com>
2023-10-13 14:02:22 +0700

    login register

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,21 +1,26 @@
+import { Link } from "@inertiajs/react"
+import React from "react"
+
 const Navbar = () => {
     return (
         <div className="navbar bg-[#17A2B8]">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl px-5 text-white">E-KOSAN</a>
+                <a className="btn btn-ghost normal-case text-xl px-8 text-white">E-KOSAN</a>
             </div>
             <div className="flex-none gap-2">
-                <div className="dropdown dropdown-end">
-                <button className="btn btn-outline text-white">Login</button>
+            <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-7 rounded-full">
+                    <img src="/images/profilelogo.png" />
+                    </div>
+                </label>
                 <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                    <li>
-                    <a className="justify-between">
-                        Login
-                    </a>
-                    </li>
-                    <li><a>Register</a></li>
+                    <>
+                    <li><Link href="/login" as="button">Login</Link></li>
+                    <li><Link href="/register" as="button">Register</Link></li>
+                    </>
                 </ul>
-                </div>
+            </div>
             </div>
         </div>
     )
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ use Inertia\Inertia;
 */
 Route::get('/', function () {
     return Inertia::render('LandingPage', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
