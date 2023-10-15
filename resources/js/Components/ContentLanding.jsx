2fa69752cea7e2bec3186f74befb740439bf864d
DaffaAriya <daffaariyaprayoga@gmail.com>
2023-10-16 02:09:57 +0700

    another components

--- resources/js/Components/ContentLanding.jsx
+++ resources/js/Components/ContentLanding.jsx
@@ -1,14 +1,13 @@
 const ContentLanding = () => {
     return (
         <>
-            <div className="hero min-h-screen bg-[#6C757D]">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img src="/images/stm3.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                    <h1 className="text-5xl font-bold text-white">Apa itu E-Kosan?</h1>
-                    <p className="py-6 text-white">E-kosan (Elektronik Kotak Saran) adalah aplikasi kotak saran digital yang memfasilitasi pengumpulan, penyimpanan, dan manajemen umpan balik, saran, atau komentar dari berbagai sumber secara elektronik. Aplikasi ini dirancang untuk memberikan saluran yang terstruktur dan efisien bagi pengguna untuk berbagi pandangan, masukan, atau keluhan mereka terkait dengan produk, layanan, atau proses tertentu. Kotak saran digital juga mencakup fitur pelaporan dan analisis data untuk membantu organisasi menganalisis dan merespons umpan balik tersebut secara efektif. Ini dapat digunakan dalam berbagai konteks, seperti perusahaan untuk meningkatkan layanan pelanggan, institusi pendidikan untuk mengumpulkan masukan siswa, atau pemerintahan untuk melibatkan masyarakat dalam pengambilan keputusan.</p>
-                    <button className="btn bg-[#17A2B8] text-white btn-outline btn-[#17A2B8] btn-xs sm:btn-sm md:btn-md lg:btn-lg]">
-                        Get Started</button>
+            <div className="hero min-h-screen" style={{backgroundImage: 'url(images/stm4.jpg)'}}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content-overlay text-center text-neutral-content">
+                    <div className="max-w-2xl">
+                    <h1 className="mb-5 text-5xl font-bold text-white">Apa itu E-Kosan?</h1>
+                    <p className="mb-5 max-w-ml text-white">E-kosan (Elektronik Kotak Saran) adalah aplikasi kotak saran digital yang memfasilitasi pengumpulan, penyimpanan, dan manajemen umpan balik, saran, atau komentar dari berbagai sumber secara elektronik. Aplikasi ini dirancang untuk memberikan saluran yang terstruktur dan efisien bagi pengguna untuk berbagi pandangan, masukan, atau keluhan mereka terkait dengan produk, layanan, atau proses tertentu. Kotak saran digital juga mencakup fitur pelaporan dan analisis data untuk membantu organisasi menganalisis dan merespons umpan balik tersebut secara efektif. Ini dapat digunakan dalam berbagai konteks, seperti perusahaan untuk meningkatkan layanan pelanggan, institusi pendidikan untuk mengumpulkan masukan siswa, atau pemerintahan untuk melibatkan masyarakat dalam pengambilan keputusan.</p>
+                    
                     </div>
                 </div>
             </div>
--- resources/js/Pages/LandingPage.jsx
+++ resources/js/Pages/LandingPage.jsx
@@ -4,6 +4,7 @@ import Footer from "@/Components/Footer";
 import { Link } from "@inertiajs/react";
 import React from "react";
 import NavbarInside from "@/Components/NavbarInside";
+import ContentSiswa from "@/Components/ContentSiswa";
 
 
 export default function LandingPage() {
