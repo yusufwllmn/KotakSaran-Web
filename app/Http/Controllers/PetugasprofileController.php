691cea093eb7142e43718a881dddcbb5e4304764
yusufwllmn <yoesoefwillman06@gmail.com>
2023-10-30 20:54:36 +0700

    add models

---
+++ app/Http/Controllers/PetugasprofileController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PetugasprofileController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,66 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+/*
+    Route::get('/', [LoginController::class, 'index'])->name('loginPage');
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+    Route::get('/register', [LoginController::class, 'registerPage'])->name('registerPage');
+    Route::post('/register', [LoginController::class, 'register'])->name('register');
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function () {
+        if(Auth::user()->role == 'pelapor'){
+            return redirect('/dashboard/');
+        } else if(Auth::user()->role == 'petugas'){
+            return redirect('/petugas/');
+        } else if(Auth::user()->role == 'admin'){
+            return redirect('/admin/');
+        }else{
+            return redirect('/');
+        }
+    });
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::middleware(['role:pelapor'])->group(function() {
+        Route::get('/dashboard', [PelaporController::class, 'index'])->name('pelaporPage');
+
+        Route::get('/laporan', [LaporanController::class, 'laporanTampil'])->name('laporan');
+        Route::post('/laporan', [LaporanController::class, 'laporanTambah'])->name('laporanTambah');
+
+        Route::get('/profile', [ProfileController::class, 'profileTampil'])->name('profile');
+        Route::put('/profile/{id_user}', [ProfileController::class, 'profileEdit'])->name('profileEdit');
+    });
+
+    Route::middleware(['role:petugas'])->group(function() {
+        Route::get('/petugas/', [PetugasController::class, 'index'])->name('petugasPage');
+
+        Route::get('/petugas/laporan', [PetugaslaporanController::class, 'laporanTampil'])->name('petugaslaporan');
+        Route::put('/petugas/laporan/{id_laporan}', [PetugaslaporanController::class, 'laporanTambah'])->name('petugaslaporanEdit');
+
+        Route::get('/petugas/profile', [PetugasprofileController::class, 'profileTampil'])->name('profile');
+        Route::put('/petugas/profile/{id_user}', [PetugasprofileController::class, 'profileEdit'])->name('petugasprofileEdit');
+
+    });
+
+    Route::middleware(['role:Customer'])->group(function() {
+        Route::get('/admin/', [AdminController::class, 'index'])->name('adminPage');
+
+        Route::get('/admin/laporan', [AdminlaporanController::class, 'laporanTampil'])->name('adminlaporan');
+        Route::post('/admin/laporan', [AdminlaporanController::class, 'laporanTambah'])->name('adminlaporanTambah');
+        Route::delete('/admin/laporan/{id_laporan}', [AdminlaporanController::class, 'laporanHapus'])->name('adminlaporanHapus');
+        Route::put('/admin/laporan/{id_laporan}', [AdminlaporanController::class, 'laporanEdit'])->name('adminlaporanEdit');
+
+        Route::get('/admin/pelapor', [AdminpelaporController::class, 'pelaporTampil'])->name('adminpelaporTampil');
+        Route::delete('/admin/pelapor/{id_user}', [AdminpelaporController::class, 'pelaporHapus'])->name('adminpelaporHapus');
+
+        Route::get('/admin/petugas', [AdminpetugasController::class, 'petugasTampil'])->name('adminpetugasTampil');
+        Route::delete('/admin/petugas/{id_user}', [AdminpetugasController::class, 'petugasHapus'])->name('adminpetugasHapus');
+
+    });
+});
+
+*/
